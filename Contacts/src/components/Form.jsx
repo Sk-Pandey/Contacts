3fa127cf31c7403bdf5c1f344ce6a95e0ba1dd26
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-06-08 15:30:37 +0530

    Form Component updated

--- Contacts/src/components/Form.jsx
+++ Contacts/src/components/Form.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <div className="flex justify-center mt-10">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">New Contact</legend>
 
-export default Form
+        <label className="label">Name</label>
+        <input type="text" className="input" placeholder="Name" />
+        <label className="label">Email</label>
+        <input type="email" className="input" placeholder="Email" />
+
+        <label className="label">Phone</label>
+        <input type="tel" className="input" placeholder="Phone" />
+
+        <button className="btn btn-soft btn-success mt-4">Save</button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Form;
